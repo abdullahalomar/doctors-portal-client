9edcd49b2a78d7a8fc9b73a0f472d32cd56a13eb
unknown <abdullahalomar048@gmail.com>
2022-05-27 17:45:24 +0600

    login form added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
        <Route path='/home' element={<Home></Home>}></Route>
         <Route path='about' element={<About></About>}></Route>
         <Route path='appointment' element={<Appointment/>}></Route>
-        <Route path='login' element={<Login></Login>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -5,9 +5,9 @@ import chair from '../../assets/images/chair.png';
 const AppointmentBanner = ({date, setDate}) => {
     
     return (
-        <div class="hero min-h-screen">
-  <div class="hero-content flex-col lg:flex-row-reverse">
-    <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt='dentist chair' />
+        <div className="hero min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='dentist chair' />
     <div>
                     <DayPicker
                      mode="single"
