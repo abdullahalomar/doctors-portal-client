bfe68880f4987f7d266295f1a90f59973e145aad
unknown <abdullahalomar048@gmail.com>
2022-05-27 18:56:00 +0600

    register added

--- src/Pages/Login/Signup.js
+++ src/Pages/Login/Signup.js
@@ -1,46 +1,72 @@
 import React from 'react';
-import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const Signup = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
     const [
-        signInWithEmailAndPassword,
+        createUserWithEmailAndPassword,
         user,
         loading,
         error,
-    ] = useSignInWithEmailAndPassword(auth);
+    ] = useCreateUserWithEmailAndPassword(auth);
+    
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+    const navigate = useNavigate()
     
     let signInError;
     
-    if (loading|| gLoading) {
+    if (loading|| gLoading || updating) {
         return <Loading></Loading>
     }
 
-    if (error || gError) {
-        signInError = <p className='text-red-500'>{ error?.message || gError?.message}</p>
+    if (error || gError || updateError) {
+        signInError = <p className='text-red-500'>{ error?.message || gError?.message || updateError?.message}</p>
     }
     
     if (user || gUser) {
         console.log(user || gUser);
     }
 
-    const onSubmit = data => {
-        console.log(data);
-        signInWithEmailAndPassword(data.email, data.password);
+    const onSubmit = async data => {
+        await createUserWithEmailAndPassword(data.email, data.password);
+        await updateProfile({ displayName: data.name });
+        console.log('update done');
+        navigate('/appointment');
     }
+
 
     return (
         <div className='flex h-screen justify-center items-center'>
            <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
-                    <h2 className="text-center text-2xl font-bold">Login</h2>
+                    <h2 className="text-center text-2xl font-bold">Sign Up</h2>
                     <form onSubmit={handleSubmit(onSubmit)}>
 
+<div class="form-control w-full max-w-xs">
+  <label class="label">
+    <span class="label-text">Name</span>
+    
+  </label>
+<input type="text" placeholder="Your Name" class="input input-bordered w-full max-w-xs"
+    {...register("name", {
+        required: {
+         value: true,
+         message: 'Name is Required'
+      }
+})}                         
+/>
+    
+   <label class="label">
+    {errors.name?.type === 'required' && <span class="label-text-alt text-red-500">{ errors.name.message}</span>}                                                    
+  </label>
+  </div>
+                        
 <div class="form-control w-full max-w-xs">
   <label class="label">
     <span class="label-text">Email</span>
@@ -91,10 +117,10 @@ const Login = () => {
 </div>
      
       {signInError}                  
-      <input className='btn w-full max-w-xs text-white' type="submit" value='Login'/>
+      <input className='btn w-full max-w-xs text-white' type="submit" value='Sign up'/>
 </form>
 
-<p><small>New to Doctors Portal <Link className='text-primary font-bold' to='/signup'>Create New Account</Link></small></p>                    
+<p><small>Already have an account? <Link className='text-primary font-bold' to='/signup'>Please Login</Link></small></p>                    
 
 <div className="divider">OR</div>
 <button onClick={() => signInWithGoogle()} className="btn btn-outline">CONTINUE WITH GOOGLE</button>
@@ -103,5 +129,5 @@ const Login = () => {
         </div>
     );
 };
-export default Login;
 
+export default Signup;
