6eb93b0cdb6d7e6fb7cc0546bb2411a4baa0360e
abdullah Al Omar <abdullahalomar048@gmail.com>
2023-03-31 17:53:10 +0600

    first

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import { Routes, Route } from 'react-router-dom';
+import Login from './Pages/Login/Login';
+
+function App() {
+  return (
+    <div>
+    <Navbar></Navbar>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='about' element={<About/>} />
+      <Route path='login' element={<Login/>} />
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
